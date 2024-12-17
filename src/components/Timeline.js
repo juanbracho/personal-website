38b6b377ee23d7580e781a67c70fff14a65152cc
Juan Bracho <juanbracho16@gmail.com>
2024-12-16 19:58:16 -0600

    .

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -5,11 +5,13 @@ import { faBriefcase, faBuildingColumns } from '@fortawesome/free-solid-svg-icon
 
 const Timeline = () => {
   const timelineData = [
+    { year: '2022-Present', role: 'Project Administrator', company: 'Atwell LLC', location: 'Dallas, TX, USA.', icon: faBriefcase },
+    { year: '2022-2022', role: 'ECC Retention Specialist', company: 'State Farm', location: 'Dallas, TX, USA.', icon: faBriefcase },
     { year: '2020-2021', role: 'Project Administrator', company: 'Avaya', location: 'Buenos Aires, Argentina', icon: faBriefcase },
     { year: '2019-2020', role: 'Store Manager', company: 'Mascotas de la Abadia', location: 'Buenos Aires, Argentina', icon: faBriefcase },
-    { year: '2018', role: 'Intern', company: 'MYRO Associates', location: 'Maracaibo, Venezuela', icon: faBriefcase },
-    { year: '2014-2018', role: 'Law School', company: 'Universidad Rafael Urdaneta', location: 'Maracaibo, Venezuela', icon: faBuildingColumns },
+    { year: '2024-2024', role: 'Bootcamp in Data Analytics', company: 'University of Texas at Austin', location: 'Austin, TX, USA.', icon: faBuildingColumns },
     { year: '2020-2022', role: 'MA in Corporate Law', company: 'Universidad Argentina de la Empresa', location: 'Buenos Aires, Argentina', icon: faBuildingColumns },
+    { year: '2014-2018', role: 'Law School', company: 'Universidad Rafael Urdaneta', location: 'Maracaibo, Venezuela', icon: faBuildingColumns },
   ];
 
   return (

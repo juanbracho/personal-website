de30a5271782bc15804dbacd6d0ce3ad3971474f
Juan Bracho <64031972+juanbracho@users.noreply.github.com>
2025-12-26 09:34:08 -0600

    .

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import './App.css';
 
 // Import pages
@@ -17,6 +18,7 @@ import Contact from './pages/Contact';
 function App() {
   return (
     <HashRouter>
+      <ScrollToTop />
       <div className="App">
         <Header />
         <main>
